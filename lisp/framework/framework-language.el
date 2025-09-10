d78fffc92383175e034b2da93ec73a1469f3f4d5
Jingke Z <jingke.zhang.zh@outlook.com>
2025-09-11 01:13:22 +0800

    Add treemacs, lsp-treemacs, and functions for beancount.

--- lisp/framework/framework-language.el
+++ lisp/framework/framework-language.el
@@ -1,27 +1,19 @@
-;;; framework-language.el --- setup completion system -*- lexical-binding: t012 -*-
+;;; framework-language.el --- setup completion system -*- lexical-binding: t -*-
 ;;; Commentary:
 
 ;;; Code:
 ;; Snippet
 (use-package yasnippet
- :ensure t
- :hook
- (prog-mode . yas-minor-mode)
- :config
- (yas-reload-all)
- ;; add company-yasnippet to company-backends
- ;; (defun company-mode/backend-with-yas (backend)
- ;;  (if (and (listp backend) (member 'company-yasnippet backend))
- ;;   backend
- ;;   (append (if (consp backend) backend (list backend))
- ;;        '(:with company-yasnippet))))
- ;; (setq company-backends (mapcar #'company-mode/backend-with-yas company-backends))
- ;; unbind <TAB> completion
- (define-key yas-minor-mode-map [(tab)]    nil)
- (define-key yas-minor-mode-map (kbd "TAB")  nil)
- (define-key yas-minor-mode-map (kbd "<tab>") nil)
- :bind
- (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
+  :ensure t
+  :hook
+  (prog-mode . yas-minor-mode)
+  :config
+  (yas-reload-all)
+  (define-key yas-minor-mode-map [(tab)]    nil)
+  (define-key yas-minor-mode-map (kbd "TAB")  nil)
+  (define-key yas-minor-mode-map (kbd "<tab>") nil)
+  :bind
+  (:map yas-minor-mode-map ("S-<tab>" . yas-expand)))
   
 (use-package yasnippet-snippets
  :ensure t
@@ -37,25 +29,43 @@
 
 ;; LSP
 (use-package lsp-mode
- :ensure t
- :init
- ;; set prefix for lsp-command-keymap (few alternatives - "C-l", "C-c l")
- (setq lsp-keymap-prefix "C-c l"
+  :ensure t
+  :init
+  (setq lsp-keymap-prefix "C-c l"
 	lsp-file-watch-threshold 500)
- :hook  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
- :commands (lsp lsp-deferred)
- :config
- (setq lsp-completion-provider :none)
- (setq lsp-headerline-breadcrumb-enable t)
- :bind
- ("C-c l s" . lsp-ivy-workspace-symbol))
+  :hook  (lsp-mode . lsp-enable-which-key-integration) ; which-key integration
+  :commands (lsp lsp-deferred)
+  :config
+  (setq lsp-completion-provider :none
+	lsp-headerline-breadcrumb-enable t
+	lsp-enable-on-type-formatting nil
+	lsp-format-buffer-on-save t
+	lsp-headerline-breadcrumb-icons-enable t))
+	
 
 (use-package lsp-ui
- :ensure t
- :config
- (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
- (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
- (setq lsp-ui-doc-position 'top))
+  :ensure t
+  :config
+  (define-key lsp-ui-mode-map [remap xref-find-definitions] #'lsp-ui-peek-find-definitions)
+  (define-key lsp-ui-mode-map [remap xref-find-references] #'lsp-ui-peek-find-references)
+  (setq lsp-ui-doc-position 'top))
+
+(use-package lsp-treemacs
+  :ensure t
+  :config
+  (lsp-treemacs-sync-mode 1)
+  :custom
+  (lsp-treemacs-theme "nerd-icons-ext")
+  :commands lsp-treemacs-errors-list)
+
+(use-package lsp-treemacs-nerd-icons
+  :ensure
+  (:host github :repo "Velnbur/lsp-treemacs-nerd-icons" :files ("*.el"))
+  :custom
+  (lsp-treemacs-theme "nerd-icons-ext")
+  :init
+  (with-eval-after-load 'lsp-treemacs
+          (require 'lsp-treemacs-nerd-icons)))
 
 (provide 'framework-language)
 ;;; framework-language.el ends here
