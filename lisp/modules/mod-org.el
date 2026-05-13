961e9dc7e3d313028763fdec6a16b4131da4458a
Chiyo <144647459+Jingke-Zhang@users.noreply.github.com>
2026-05-13 14:20:47 +0900

    Tidy Org mode configuration

--- lisp/modules/mod-org.el
+++ lisp/modules/mod-org.el
@@ -7,22 +7,24 @@
 (use-package org
   :ensure nil
   :hook ((org-mode . org-indent-mode)
-         (org-mode . variable-pitch-mode))
+         (org-mode . mixed-pitch-mode))
+  :custom
+  (org-hide-emphasis-markers t)
+  (org-startup-with-inline-images t)
+  (org-image-actual-width nil)
+  (org-fontify-done-headline t)
+  (org-fontify-whole-heading-line t)
+  (org-fontify-quote-and-verse-blocks t)
+  (org-adapt-indentation nil)
+  (org-src-tab-acts-natively t)
+  (org-src-fontify-natively t)
+  (org-preview-latex-default-process 'dvisvgm)
   :config
-  (setq org-hide-emphasis-markers t
-        org-startup-with-inline-images t
-        org-image-actual-width nil
-        org-fontify-done-headline t
-        org-fontify-whole-heading-line t
-        org-fontify-quote-and-verse-blocks t
-        org-adapt-indentation nil
-        org-src-tab-acts-natively t
-        org-src-fontify-natively t)
   (setq org-format-latex-options 
-        (plist-put org-format-latex-options :scale 1.5)
-        org-preview-latex-default-process 'dvisvgm)
+        (plist-put org-format-latex-options :scale 1.5))
   
-  (custom-set-faces
+  (custom-theme-set-faces
+   'user
    '(org-level-1 ((t (:inherit outline-1 :height 1.4 :weight bold))))
    '(org-level-2 ((t (:inherit outline-2 :height 1.2 :weight bold))))
    '(org-level-3 ((t (:inherit outline-3 :height 1.1 :weight bold))))
@@ -34,9 +36,9 @@
   :hook (org-mode . org-modern-mode)
   :config
   (setq org-modern-star '("◉" "○" "◈" "◇" "✳"))
-  (setq org-modern-list '((?- . "•") (?+ . "◦")))
-  (setq org-modern-table nil)
-  (setq org-modern-block-fringe nil))
+  (setq org-modern-list '((?- . "•") (?+ . "◦"))
+        org-modern-table nil
+        org-modern-block-fringe nil))
 
 (use-package org-appear
   :after org
@@ -56,8 +58,8 @@
          ;; Dailies
          ("C-c n j" . org-roam-dailies-capture-today))
   :config
-  ;; If you're using a vertical completion framework, you might want a more informative completion interface
-  (setq org-roam-node-display-template (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
+  (setq org-roam-node-display-template
+        (concat "${title:*} " (propertize "${tags:10}" 'face 'org-tag)))
   (org-roam-db-autosync-mode)
   ;; If using org-roam-protocol
   (require 'org-roam-protocol))
